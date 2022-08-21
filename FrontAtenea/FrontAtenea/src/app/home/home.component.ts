ab0ba04c129b39c00f930ba61269fdcfc33588b7
Katherine Colina <katherine@ateneamercantil.com>
2022-08-21 15:36:12 -0400

    nav bar

--- FrontAtenea/FrontAtenea/src/app/home/home.component.ts
+++ FrontAtenea/FrontAtenea/src/app/home/home.component.ts
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   navBarText = ['En Curso', 'Completo', 'Incompleto', 'Explorar'];
+  active!: number;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  activeItem(item: any) {
+    console.log('item', item);
+    this.active = item;
+  }
 }
